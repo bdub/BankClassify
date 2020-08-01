e520c8ccdb7021176043f4de0ecf4ec5f578dd2b
Ben Ward <ben@forward.net.au>
2020-08-01 16:29:33 +1000

    completed data/ME_2019_12-11-10.csv

--- Classify.py
+++ Classify.py
@@ -1,8 +1,36 @@
 from BankClassify import BankClassify
 
 #bc = BankClassify()
-bc = BankClassify(data="STG_AllData.csv")
+#bc = BankClassify(data="STG_AllData.csv")
+bc = BankClassify(data="ME_AllData.csv")
 
-#bc.add_data("85561768_20205411_0903.csv", "lloyds")
-#bc.add_data("data/STG_[112-879 099143271] Visa Freedom_2017.07.01-2020.06.30.csv", "stgeorge")
-bc.add_data("data/STG_snippet_2020_04-03.csv", "stgeorge")
+# ST GEORGE
+#bc.add_data("data/STG_snippet.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2020_04-03.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2020_02-01.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2019_12-11-10.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2019_9-8-7.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2019_6-5-4.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2019_3-2-1.csv", "stgeorge") 
+#bc.add_data("data/STG_snippet_2018_12-11-10.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2018_9-8-7.csv", "stgeorge")
+#bc.add_data("data/STG_snippet_2018_6-5-4.csv", "stgeorge") # RE-DOING - DONE
+# SUBMITTED #bc.add_data("data/STG_snippet_2018_3-2-1.csv", "stgeorge")
+# SUBMITTED #bc.add_data("data/STG_snippet_2017_12-11-10.csv", "stgeorge")
+# SUBMITTED bc.add_data("data/STG_snippet_2017_9-8-7.csv", "stgeorge")
+
+# ME BANK 
+#bc.add_data("data/ME_snippet_v2.csv", "mebank")
+#bc.add_data("data/ME_2020_6.csv", "mebank")
+#bc.add_data("data/ME_2020_T5.csv", "mebank")
+#bc.add_data("data/ME_2020_5_save.csv", "mebank")
+#bc.add_data("data/ME_2020_4.csv", "mebank")
+#bc.add_data("data/ME_2020_3-2-1.csv", "mebank")
+#bc.add_data("data/ME_2019_12-11-10.csv", "mebank") 
+bc.add_data("data/ME_2019_9-8-7.csv", "mebank")
+#bc.add_data("data/ME_2019_6-5-4.csv", "mebank")
+#bc.add_data("data/ME_2019_3-2-1.csv", "mebank") # segments
+#bc.add_data("data/ME_2018_12-11-10.csv", "mebank") # segments
+#bc.add_data("data/ME_2018_9-8-7.csv", "mebank") # segments
+#bc.add_data("data/ME_2018_6-5-4.csv", "mebank") # REDOING 
+
